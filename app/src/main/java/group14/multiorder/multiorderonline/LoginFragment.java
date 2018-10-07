4395f87c2bc792f8fa3bd1ab39bd7d3cb6d0e80e
Bright <59070035@it.kmitl.ac.th>
2018-10-07 13:09:27 +0700

    add loginFragment [font,border,bg,logo]

--- app/src/main/java/group14/multiorder/multiorderonline/LoginFragment.java
+++ app/src/main/java/group14/multiorder/multiorderonline/LoginFragment.java
@@ -1,5 +1,6 @@
 package group14.multiorder.multiorderonline;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginFragment extends Fragment {
     @Nullable
@@ -15,9 +19,23 @@ public class LoginFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
+    EditText userName;
+    EditText pwd;
+    Button btn;
+    Typeface typeface;
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        setFont();
+    }
 
+    public void setFont(){
+        typeface = Typeface.createFromAsset(getActivity().getAssets(), "font/Franchise-Bold.ttf");
+        userName = getActivity().findViewById(R.id.login_username);
+        pwd = getActivity().findViewById(R.id.login_pwd);
+        btn = getActivity().findViewById(R.id.login_login);
+        userName.setTypeface(typeface);
+        pwd.setTypeface(typeface);
+        btn.setTypeface(typeface);
     }
 }
