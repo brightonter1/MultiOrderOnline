20251477b583639d5d3aa20035c4923931c89986
enjoy <59070049@it.kmitl.ac.th>
2018-10-10 17:59:45 +0700

    delete duplicate method

--- app/src/main/java/group14/multiorder/multiorderonline/MainActivity.java
+++ app/src/main/java/group14/multiorder/multiorderonline/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null){
+
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.main_view, new LoginFragment())
