2fe0e6b5ccc9494f04bb55825d59a935072e9d55
enjoy <59070049@it.kmitl.ac.th>
2018-10-10 16:58:19 +0700

    delete duplicate method

--- app/src/main/java/group14/multiorder/multiorderonline/LoginFragment.java
+++ app/src/main/java/group14/multiorder/multiorderonline/LoginFragment.java
@@ -101,39 +101,4 @@ public class LoginFragment extends Fragment {
         pwd.setTypeface(typeface);
         btn.setTypeface(typeface);
     }
-    public void btnLogin(){
-        Button btnLogin = getActivity().findViewById(R.id.login_login);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                userName = getView().findViewById(R.id.login_username);
-                pwd = getView().findViewById(R.id.login_pwd);
-                _userStr = userName.getText().toString();
-                _pwdStr = pwd.getText().toString();
-                if (_userStr.isEmpty() || _pwdStr.isEmpty()){
-                    Log.d("System", "[Login] username or password is empty");
-                    Toast.makeText(getActivity(), "Please fill your info", Toast.LENGTH_SHORT).show();
-                }else{
-                    mAuth.signInWithEmailAndPassword(_userStr,_pwdStr).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                        @Override
-                        public void onSuccess(AuthResult authResult) {
-                            if (authResult.getUser().isEmailVerified()){
-                                Log.d("System", "[Login] login complete");
-                            }
-                            else if (authResult.getUser().isEmailVerified() == false){
-                                Log.d("System", "[Login] email not verified");
-                                Toast.makeText(getActivity(), "Please confirm your email", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d("System", "[Login] Invalid " + e.getMessage());
-                            Toast.makeText(getActivity(), "Invalid user or password", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-            }
-        });
-    }
 }
