af59f8b51c3339da5d91534db94a373ea2e798b2
Bright <59070035@it.kmitl.ac.th>
2018-10-17 11:32:29 +0700

    ทดลองดึงข้อมูลและสร้างฐานข้อมูลสำหรับผู้ใช้

--- app/src/main/java/group14/multiorder/multiorderonline/LoginActivity.java
+++ app/src/main/java/group14/multiorder/multiorderonline/LoginActivity.java
@@ -35,15 +35,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
         context = this;
-//        setFont();
-//        btnLogin();
-//        btnRegister();
-        if (savedInstanceState == null){
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.main_view, new RoleSelectorFragment())
-                    .commit();
-        }
+        setFont();
+        btnLogin();
+        btnRegister();
+//        if (savedInstanceState == null){
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.main_view, new RoleSelectorFragment())
+//                    .commit();
+//        }
     }
     public void btnRegister(){
         TextView btnReg = findViewById(R.id.login_register);
--- app/src/main/java/group14/multiorder/multiorderonline/RegisterActivity.java
+++ app/src/main/java/group14/multiorder/multiorderonline/RegisterActivity.java
@@ -11,16 +11,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class RegisterActivity extends AppCompatActivity {
-
     private FirebaseAuth mAuth;
     private Context context;
+    private ArrayList<String> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,9 +67,11 @@ public class RegisterActivity extends AppCompatActivity {
                     sendVerifiedEmail(authResult.getUser());
                     Log.d("System", "[Register] Register Complete");
                     Toast.makeText(context, "Register Complete!!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                    finish();
-                    startActivity(intent);
+                    createDBforUser(mAuth.getCurrentUser().getUid());
+                    pullDBallUser(mAuth.getCurrentUser().getUid());
+//                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+//                    finish();
+//                    startActivity(intent);
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
@@ -63,6 +81,85 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+    public void pullDBallUser(String _uid){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        DocumentReference docRef = db.collection("buyer").document(mAuth.getCurrentUser().getUid());
+//        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if (task.isSuccessful()){
+//                    DocumentSnapshot doc = task.getResult();
+//                    if (doc.exists()){
+//                        Log.d("pull", String.valueOf(doc.getData()));
+//                    }else
+//                        Log.d("pull", "No Such");
+//                }else{
+//                    Log.d("pull", "get failed with ", task.getException());
+//                }
+//            }
+//        });
+
+        db.collection("buyer")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("pull", document.getId() + " => " + document.getData());
+                                test(document.getId());
+                            }
+                        } else {
+                            Log.d("pull", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+    }
+    public void test(String _uid){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference doc = db.collection("buyer").document(_uid);
+        doc.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w("pull", "Listen failed.", e);
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    if (snapshot.get("name") != null){
+                        Log.d("pull", "Name data: " + snapshot.get("name"));
+                        Log.d("pull", "Lastname data: " + snapshot.get("lastname"));
+                        Log.d("pull", "Age data: " + snapshot.get("age"));
+                    }
+                } else {
+                    Log.d("pull", "Current data: null");
+                }
+            }
+        });
+    }
+    public void createDBforUser(String _uid){
+        Map<String, String> data = new HashMap<>();
+        data.put("name", "BBBBRIGHT");
+        data.put("lastname", "123456");
+        data.put("age", "21");
+        FirebaseFirestore _fireStore = FirebaseFirestore.getInstance();
+        _fireStore.collection("buyer").document(_uid).set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("System", "Done");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("System", "Faild");
+            }
+        });
+
+
     }
     public Boolean checkPwdCondition(String _pwd){
         if (_pwd.length() >= 6){
