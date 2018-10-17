3345d8dd4c993cb0c91c798f319007f357890959
ponponok <59070017@it.kmitl.ac.th>
2018-10-17 22:21:48 +0700

    Compressing image

--- app/src/main/java/group14/multiorder/multiorderonline/PostFragment.java
+++ app/src/main/java/group14/multiorder/multiorderonline/PostFragment.java
@@ -3,7 +3,9 @@ package group14.multiorder.multiorderonline;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -15,10 +17,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import group14.multiorder.multiorderonline.util.UniversalImageLoader;
 
@@ -53,6 +59,7 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
     //vars
     private Bitmap mSelectedBitmap;
     private Uri mSelectedUri;
+    private byte[] mUploadBytes;
 
 
     @Nullable
@@ -74,14 +81,14 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
 
-        init();
+        SelectImageInit();
 
         return view;
     }
 
 
 
-    private void init(){
+    private void SelectImageInit(){
 
         mPostImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,6 +99,86 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
                 dialog.setTargetFragment(PostFragment.this, 1);
             }
         });
+    }
+
+    private void PostInit(){
+        mPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Post image ");
+                if(!isEmpty(mTitle.getText().toString())
+                        && !isEmpty(mDescription.getText().toString())
+                        && !isEmpty(mPrice.getText().toString())
+                        ){
+                    //have Bitmap no Uri
+                    if(mSelectedBitmap != null && mSelectedUri == null){
+                        UploadImage(mSelectedBitmap);
+                    }
+                    //have Uri no Bitmap
+                    else if(mSelectedBitmap == null && mSelectedUri != null){
+                        UploadImage(mSelectedUri);
+                    }
+
+                } else{
+                    Toast.makeText(getActivity(), "Please fill out all the fields", Toast.LENGTH_SHORT  ).show();
+                }
+            }
+        });
+    }
+
+    private void UploadImage(Bitmap bitmap){
+
+    }
+
+    private void UploadImage(Uri imagePath){
+
+    }
+
+    public class BackgroundImageResize extends AsyncTask<Uri, Integer, byte[]>{
+        Bitmap mBitmap;
+
+        BackgroundImageResize(Bitmap bitmap){
+            this.mBitmap = bitmap;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getActivity(), "Compressing Image", Toast.LENGTH_SHORT).show();
+            showProgressBar();
+        }
+
+
+        @Override
+        protected byte[] doInBackground(Uri... uris) {
+            Log.d(TAG, "doInBackground: Strat");
+
+            if(mBitmap == null){
+                try {
+                    mBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uris[0]);
+
+                }catch (IOException e){
+                    Log.e(TAG, "doInBackground: IOException: "+ e.getMessage());
+                }
+            }
+            byte[] bytes = null;
+            bytes = getBytesFromBitmap(mBitmap, 100);
+            return bytes;
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            super.onPostExecute(bytes);
+            mUploadBytes = bytes;
+            hideProgressBar();
+            //execute the upload task
+        }
+    }
+
+    public static  byte[] getBytesFromBitmap(Bitmap bitmap, int quality){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream);
+        return stream.toByteArray();
     }
 
     private void resetFields(){
