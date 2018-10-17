12a1556cd05b84df9de35b02dc614e384dfcb3b9
ponponok <59070017@it.kmitl.ac.th>
2018-10-18 00:12:48 +0700

    upload image

---
+++ app/src/main/java/group14/multiorder/multiorderonline/Post.java
@@ -0,0 +1,84 @@
+package group14.multiorder.multiorderonline;
+
+public class Post {
+
+    private String post_id;
+    private String user_id;
+    private String image;
+    private String title;
+    private String description;
+    private String price;
+
+    public Post(){}
+
+    public Post(String post_id, String user_id, String image,
+                String title, String description, String price){
+        this.setPost_id(post_id);
+        this.setUser_id(user_id);
+        this.setImage(image);
+        this.setTitle(title);
+        this.setDescription(description);
+        this.setPrice(price);
+    }
+
+
+    public String getPost_id() {
+        return post_id;
+    }
+
+    public void setPost_id(String post_id) {
+        this.post_id = post_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "post{" +
+                "post_id='" + post_id + '\'' +
+                ", user_id='" + user_id + '\'' +
+                ", image='" + image + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/group14/multiorder/multiorderonline/PostFragment.java
+++ app/src/main/java/group14/multiorder/multiorderonline/PostFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -20,6 +21,16 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
@@ -60,6 +71,7 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
     private Bitmap mSelectedBitmap;
     private Uri mSelectedUri;
     private byte[] mUploadBytes;
+    private double mProgress = 0;
 
 
     @Nullable
@@ -82,6 +94,7 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
 
 
         SelectImageInit();
+        PostInit();
 
         return view;
     }
@@ -127,10 +140,16 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
     }
 
     private void UploadImage(Bitmap bitmap){
-
+        Log.d(TAG, "uploadNewPhoto: uploading a new image bitmap to storage");
+        BackgroundImageResize resize = new BackgroundImageResize(bitmap);
+        Uri uri = null;
+        resize.execute(uri);
     }
 
     private void UploadImage(Uri imagePath){
+        Log.d(TAG, "uploadNewPhoto: uploading a new image uri to storage");
+        BackgroundImageResize resize = new BackgroundImageResize(null);
+        resize.execute(imagePath);
 
     }
 
@@ -162,7 +181,9 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
                 }
             }
             byte[] bytes = null;
+            Log.d(TAG, "doInBackground: megabytes before compression: "+ mBitmap.getByteCount()/1000000);
             bytes = getBytesFromBitmap(mBitmap, 100);
+            Log.d(TAG, "doInBackground: megabytes before compression: "+ bytes.length/1000000);
             return bytes;
         }
 
@@ -172,7 +193,61 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
             mUploadBytes = bytes;
             hideProgressBar();
             //execute the upload task
+            excecuteUploadTask();
         }
+    }
+
+    private void excecuteUploadTask(){
+        Toast.makeText(getActivity(), "uploading image", Toast.LENGTH_SHORT).show();
+        final String postId = FirebaseDatabase.getInstance().getReference().push().getKey();
+
+        final StorageReference  storageReference = FirebaseStorage.getInstance().getReference()
+                .child("posts/users/" + FirebaseAuth.getInstance().getCurrentUser().getUid() +
+                "/" + postId + "/post_image");
+
+        UploadTask uploadTask = storageReference.putBytes(mUploadBytes);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getActivity(), "Post Success", Toast.LENGTH_SHORT).show();
+
+                //insert the download uri into firebase database
+                Task<Uri> task = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+                Log.d(TAG, "OnSuccess: firebase download url"+ task.toString());
+                //Uri firebaseUri = taskSnapshot.getMetadata().getReference().getDownloadUrl().getResult();
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+                Post post = new Post();
+                post.setImage(task.toString());
+                post.setDescription(mDescription.getText().toString());
+                post.setPost_id(postId);
+                post.setPrice(mPrice.getText().toString());
+                post.setTitle(mTitle.getText().toString());
+                post.setUser_id(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                reference.child(getString(R.string.node_post))
+                        .child(postId)
+                        .setValue(post);
+                resetFields();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "could not upload photo", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double currentProgress = (100*taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                if(currentProgress > (mProgress+15)){
+                    mProgress = (100*taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    Log.d(TAG, "onProgress: upload is "+ mProgress+ "& done");
+                    Toast.makeText(getActivity(), mProgress + "%", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 
     public static  byte[] getBytesFromBitmap(Bitmap bitmap, int quality){
