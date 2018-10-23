899b67ada90183d8924ef513f5519949efd54695
enjoy <59070049@it.kmitl.ac.th>
2018-10-23 13:31:23 +0700

    completed role selector

--- app/src/main/java/group14/multiorder/multiorderonline/RoleSelectorActivity.java
+++ app/src/main/java/group14/multiorder/multiorderonline/RoleSelectorActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -28,29 +29,29 @@ public class RoleSelectorActivity extends AppCompatActivity{
     }
 
     public void suplierButton(){
-        Button _supplierBtn = findViewById(R.id.role_selector_suplier_btn);
+        ImageView _supplierBtn = findViewById(R.id.role_selector_suplier_img);
         _supplierBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
 //
-//                Intent intent = new Intent(RoleSelectorActivity.this, SuplierRegisterActivity.class);
-//                finish();
-//                startActivity(intent);
+                Intent intent = new Intent(RoleSelectorActivity.this, SuplierRegisterActivity.class);
+                finish();
+                startActivity(intent);
 //                go to suplieractivity
             }
         });
     }
 
     public void customerButton(){
-        Button _customerBtn = findViewById(R.id.role_selector_customer_btn);
+        ImageView _customerBtn = findViewById(R.id.role_selector_customer_img);
         _customerBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
 
-//                Intent intent = new Intent(RoleSelectorActivity.this, RegisterActivity.class);
-//                finish();
-//                startActivity(intent);
+                Intent intent = new Intent(RoleSelectorActivity.this, RegisterActivity.class);
+                finish();
+                startActivity(intent);
 //                go to customer activity
             }
         });
