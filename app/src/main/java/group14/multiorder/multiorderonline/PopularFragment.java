498fa37c495e55da5f4ffd31ae0c169c8d629e50
Bright <59070035@it.kmitl.ac.th>
2018-10-23 22:10:49 +0700

    no message

--- app/src/main/java/group14/multiorder/multiorderonline/PopularFragment.java
+++ app/src/main/java/group14/multiorder/multiorderonline/PopularFragment.java
@@ -2,10 +2,14 @@ package group14.multiorder.multiorderonline;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -26,4 +30,16 @@ public class PopularFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_popular, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button btn = getActivity().findViewById(R.id.btn_click);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+            }
+        });
+    }
 }
