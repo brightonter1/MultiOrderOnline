a6251b7682c9e99af01fc0afc83c3349d7f13f83
yimmf95 <59070033@it.kmitl.ac.th>
2018-11-28 22:52:05 +0700

    process

--- app/src/main/java/group14/multiorder/multiorderonline/Cart/CartSummaryFragment.java
+++ app/src/main/java/group14/multiorder/multiorderonline/Cart/CartSummaryFragment.java
@@ -5,15 +5,35 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import group14.multiorder.multiorderonline.R;
+import group14.multiorder.multiorderonline.obj.Menu;
 
 public class CartSummaryFragment extends Fragment{
+    private Cart cart;
+    private List<Menu> _menuList = new ArrayList<Menu>();
+    private FirebaseAuth _mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore _fileStore = FirebaseFirestore.getInstance();
+
+    private RecyclerView sumView;
+    private CartSummaryAdapter sumAdapter;
 
     private Toolbar toolbar;
     public CartSummaryFragment(){}
@@ -33,8 +53,36 @@ public class CartSummaryFragment extends Fragment{
         TextView title = getActivity().findViewById(R.id.title_bar);
         title.setText("Summary");
         backBtn();
+        showMenu();
     }
+    private void showMenu(){
+        sumView = getView().findViewById(R.id.summary_items);
+        sumView.setHasFixedSize(true);
+        sumView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        cart = new Cart();
+        _fileStore.collection("carts").document(_mAuth.getCurrentUser().getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    cart = documentSnapshot.toObject(Cart.class);
+                    ArrayList<Menu> mm = cart.get_menuList();
+                    for(Menu aaa : mm){
+                        //Log.d("CART","Add "+ aaa.getTitle());
+                        _menuList.add(aaa);
+                    }
+                    sumAdapter = new CartSummaryAdapter(getActivity(), _menuList);
+                    sumView.setAdapter(sumAdapter);
+                    TextView total = getView().findViewById(R.id.summary_total);
+                    total.setText("Total " + String.valueOf(cart.getTotal())+"฿");
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
 
+            }
+        });
+    }
 
 
     public void backBtn(){
