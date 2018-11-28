340eeec8c803e75587992e5ad298344f04913f56
brightonter1 <59070035@it.kmitl.ac.th>
2018-11-28 22:50:59 +0700

    no message

---
+++ app/src/main/java/group14/multiorder/multiorderonline/MyApp.java
@@ -0,0 +1,14 @@
+package group14.multiorder.multiorderonline;
+
+
+import android.support.v7.widget.Toolbar;
+
+public interface  MyApp {
+    public Toolbar toolbar = null;
+
+   public void setToolbar();
+   public void backBtn();
+
+
+
+}
