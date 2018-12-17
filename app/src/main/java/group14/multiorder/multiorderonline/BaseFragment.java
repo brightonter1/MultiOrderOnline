12df00289a05483397159a05195201312c693110
brightonter1 <59070035@it.kmitl.ac.th>
2018-12-17 16:32:08 +0700

    Keep type of user

---
+++ app/src/main/java/group14/multiorder/multiorderonline/BaseFragment.java
@@ -0,0 +1,22 @@
+package group14.multiorder.multiorderonline;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public abstract class BaseFragment extends Fragment {
+    public Toolbar toolbar = null;
+
+    public abstract void backBtn();
+
+    public void setToolbar(String nPager){
+        toolbar = getActivity().findViewById(R.id.account_bar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        TextView title = getActivity().findViewById(R.id.title_bar);
+        title.setText(nPager);
+    }
+}
